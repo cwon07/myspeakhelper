0ce0216d2116d0f490fd66352c99731a14f7ea33
cwon07 <jumiryoo@gmail.com>
2025-06-09 23:10:02 -0700

    added the missing type='button' for the logout button

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -54,6 +54,7 @@ export default function Layout({ children }: Props) {
                             <Link
                                 href="/login"
                                 className='ml-4 bg-blue-500 text-white px-3 py-1 rounded hover:bg-blue-600'
+                                type='button'
                             >
                                 Log In
                             </Link>
